f1a0ad2d9419bfcf8bfd8eff5d5a2e5bc4ca91fc
Peter Halliday <pghalliday@gmail.com>
2015-12-01 14:38:55 +0100

    parameterize filebeat and sensu client specific options

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
 # the following attribute should be set to the contents of a standard
 # Formatron configuration data bag item
 default['formatron_common']['configuration'] = nil
+
+# override these as required
+default['formatron_common']['filebeat']['paths'] = ['/var/log/**/*.log']
+default['formatron_common']['sensu']['subscriptions'] = ['default']
--- recipes/filebeat.rb
+++ recipes/filebeat.rb
@@ -1,8 +1,9 @@
 configuration = node['formatron_common']['configuration']
+paths = node['formatron_common']['filebeat']['paths']
 hosted_zone_name = configuration['dsl']['global']['hosted_zone_name']
 
 elk_config = configuration['config']['elk']
 node.override['formatron_filebeat']['logstash']['hostname'] = "#{elk_config['sub_domain']}.#{hosted_zone_name}"
 node.override['formatron_filebeat']['logstash']['port'] = elk_config['logstash_port']
-node.override['formatron_filebeat']['paths'] = ['/var/log/**/*.log']
+node.override['formatron_filebeat']['paths'] = paths
 include_recipe 'formatron_filebeat::default'
