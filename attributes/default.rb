0d7fba129b058fced9b6a53fe27f7885f484f0eb
Peter Halliday <pghalliday@gmail.com>
2015-12-05 10:18:48 +0100

    disable packetbeat and topbeat

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,8 @@ default['formatron_common']['configuration'] = nil
 # override these as required
 default['formatron_common']['filebeat']['paths'] = ['/var/log/**/*.log']
 default['formatron_common']['sensu']['subscriptions'] = ['default']
+
+# enable/disable beats (some are not very stable)
+override['formatron_filebeat']['enabled'] = true
+override['formatron_packetbeat']['enabled'] = false
+override['formatron_topbeat']['enabled'] = false
