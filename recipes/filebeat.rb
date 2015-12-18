bd0beda6f7cb573abe5651b49cbcdb8b46911d18
Peter Halliday <pghalliday@gmail.com>
2015-12-18 12:00:11 +0100

    use prospector LWRP

--- recipes/filebeat.rb
+++ recipes/filebeat.rb
@@ -5,3 +5,10 @@ logstash_config = configuration['config']['logstash']
 node.default['formatron_filebeat']['logstash']['host'] = "#{logstash_config['sub_domain']}.#{hosted_zone_name}"
 node.default['formatron_filebeat']['logstash']['port'] = logstash_config['beats_port']
 include_recipe 'formatron_filebeat::default'
+
+formatron_filebeat_prospector 'syslog' do
+  document_type 'syslog'
+  paths [
+    '/var/log/syslog'
+  ]
+end
